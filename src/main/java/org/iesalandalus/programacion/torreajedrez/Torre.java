76ffa2eeeb78772f5fad9264434a53ef340a17ef
Konstantin Krishtapov <constantinopladwar@gmail.com>
2021-11-17 17:15:49 +0100

    Creo metodos getter y setter para clase torre

--- src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
@@ -1,10 +1,35 @@
 package org.iesalandalus.programacion.torreajedrez;
 
 public class Torre {
-	
-	//Atributos
-	
+
+	// Atributos
+
 	private Color color;
 	private Posicion posicion;
+
+	
+	//Creo metodos Getters y Setters
+	private void setPosicion(Posicion posicion) {
+
+		this.posicion = posicion;
+		
+	}
+
+	public Posicion getPosicion() {
+
+		return this.posicion;
+	}
+
+	private void setColor(Color color) {
+
+		if (color == null) {
+			throw new IllegalArgumentException("ERROR: No se puede asignar un color nulo.");
+		}
+		this.color = color;
+	}
+
+	public Color getColor() {
+		return this.color;
+	}
 
 }
