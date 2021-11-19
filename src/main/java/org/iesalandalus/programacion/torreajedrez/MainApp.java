12631ee8b679a25d8043a0b842fa2024a9b2fb46
Konstantin Krishtapov <constantinopladwar@gmail.com>
2021-11-19 14:39:52 +0100

    Creo metodo para mostrar torre

--- src/main/java/org/iesalandalus/programacion/torreajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/MainApp.java
@@ -1,7 +1,21 @@
 package org.iesalandalus.programacion.torreajedrez;
 
 public class MainApp {
-
+	
+	private static Torre torre;
+	private static boolean salir;
+	
+	
+	//Creando metodo para mostrar Torre
+	
+	private void mostrarTorre() {
+		
+		if(torre != null && salir == false) {
+			
+			System.out.println(torre);
+		}
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("kk");
 	}
